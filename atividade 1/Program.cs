f806335319c21bc74176cdab67a80b5a8639171d
felip <felip@DESKTOP-0VT61A2>
2023-03-26 13:57:34 -0300

    Adicionar arquivos de projeto.

---
+++ atividade 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor entre 1 e 9:");
+        int valor = int.Parse(Console.ReadLine());
+
+        if (valor >= 1 && valor <= 9)
+        {
+            Console.WriteLine("O valor está na faixa permitida");
+        }
+        else
+        {
+            Console.WriteLine("O valor está fora da faixa permitida");
+        }
+    }
+}
+
+//atividade 1//
